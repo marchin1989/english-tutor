c18a3a3282944f0338af1c775a34601e4b4e4043
Masayuki Abe <10809130+marchin1989@users.noreply.github.com>
2023-12-01 21:50:49 +0900

    Add trying to use openai with langchain

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # english tutor
+
+```shell
+# openai api keyの設定
+export OPENAI_API_KEY="..."
+
+# 実行
+poetry run python app.py
+```
--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-print("Hello World")
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.schema import HumanMessage
+
+MODEL_NAME = "gpt-4"
+
+prompt = PromptTemplate.from_template("""
+Please correct any unnatural English in the following conversation.
+And then, explain in Japanese how unnatural it is.
+
+===
+{conversation}
+===
+""")
+
+
+def chat_invoke(messages):
+    chat_model = ChatOpenAI(model=MODEL_NAME)
+    return chat_model.invoke(messages)
+
+
+if __name__ == "__main__":
+    conversation = """
+    A: What kinds of music do you like?
+    B: I likes classic music.
+    """
+
+    messages = [HumanMessage(content=prompt.format(conversation=conversation))]
+
+    print(chat_invoke(messages))
